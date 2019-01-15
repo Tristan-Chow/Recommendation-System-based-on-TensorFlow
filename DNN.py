5eb94fc6b40c11f8e49b18f43f518a245813958a
TianyiZhou <32355665+Tristan-Chow@users.noreply.github.com>
2019-01-15 09:51:49 -0800

    Update DNN.py

--- DNN.py
+++ DNN.py
@@ -16,7 +16,7 @@ class DNN:
         self.geo = tf.placeholder(tf.int64, [None, ])
         self.wh = tf.placeholder(tf.int64, [None, ])
         self.time = tf.placeholder(tf.int64, [None, ])
-        self.y = tf.placeholder(tf.int64, [None])
+        self.y = tf.placeholder(tf.int64, [None, ])
 
         self.genre_Query = genre_matrix
 
@@ -27,28 +27,29 @@ class DNN:
         with tf.name_scope("embedding"):
             watch_genre = tf.gather(self.genre_Query, self.wh)
             watch_history_genre_emb = tf.nn.embedding_lookup(self.genre_emb_w, watch_genre)
-            watch_history_final = tf.reduce_mean(tf.reduce_mean(watch_history_genre_emb, axis=0), axis=0)
+            watch_history_final = tf.reduce_mean(watch_history_genre_emb, axis=1)
 
             geo_emb = tf.nn.embedding_lookup(self.geographic_emb_w, self.geo)
             occ_emb = tf.nn.embedding_lookup(self.occupation_emb_w, self.o)
 
-        user_vector = tf.concat([geo_emb, occ_emb, watch_history_final, self.g, self.a, self.time], axis=0)
+        user_vector = tf.concat([geo_emb, occ_emb, watch_history_final, tf.stack([self.g, self.a, self.time], axis=1)],
+                                axis=1)
 
         # Deep Neural Network
-        with tf.name_scope("DNN"):
-            d_layer_1 = tf.layers.Dense(user_vector, units=1024, activation=tf.nn.relu, use_bias=True, name='f1',
+        with tf.name_scope("layer"):
+            d_layer_1 = tf.layers.dense(user_vector, units=1024, activation=tf.nn.relu, use_bias=True, name='f1',
                                         trainable=True)
-            d_layer_2 = tf.layers.Dense(d_layer_1, units=512, activation=tf.nn.relu, use_bias=True, name='f2',
+            d_layer_2 = tf.layers.dense(d_layer_1, units=512, activation=tf.nn.relu, use_bias=True, name='f2',
                                         trainable=True)
-            d_layer_3 = tf.layers.Dense(d_layer_2, units=256, activation=tf.nn.relu, use_bias=True, name='f3',
+            d_layer_3 = tf.layers.dense(d_layer_2, units=256, activation=tf.nn.relu, use_bias=True, name='f3',
                                         trainable=True)
         movie_embedding = tf.Variable(
             tf.truncated_normal([movie_count, embedding_size], steddev=1.0 / math.sqrt(embedding_size)), trainable=True)
-        biase = tf.Variable(tf.zeros([movie_count]))
+        biases = tf.Variable(tf.zeros([movie_count]))
 
         with tf.name_scope("loss"):
             self.loss = tf.reduce_mean(
-                tf.nn.sampled_softmax_loss(inputs=d_layer_3, weights=movie_embedding, biases=biase,
-                                           num_classes=movie_count,
+                tf.nn.sampled_softmax_loss(inputs=d_layer_3, weights=movie_embedding, biases=biases,
+                                           num_classes=movie_count, labels=self.y
                                            num_true=1))
 
